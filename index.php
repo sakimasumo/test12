8afd7881d67b28bd25e2f3c5c15fe052208088b1
SakikoMasumoto <zd3h10@gmail.com>
2020-04-03 16:05:29 +0900

    1st

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+session_start();
+$id = "";
+$pass = "";
+
+if ($_SESSION["authenticated"] == true) {
+    header("Location: memberonly.php");
+    exit;
+}
+
+$user = "";
+
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $user = $_POST["user"];
+    $pass = $_POST["pass"];
+    if ($user === "taro" and $pass === "abcd") {
+        $_SESSION["authenticated"] = true;
+        $_SESSION["userid"] = $user;
+        header("Location: memberonly.php");
+        exit;
+    }else {
+        $loginError = "ユーザIDかパスワードがただしくありません";
+    }
+}
+
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ログイン</title>
+</head>
+<body>
+    <h1>ログイン</h1>
+    <?php if(isset($loginError)): ?>
+    <p　class="error"><?= $loginError ?></p>
+    <?php endif; ?>
+    <p>ユーザIDとパスワードを入力して「ログイン」ボタンを押してください</p>
+    <form action="" method="post">
+        <table>
+        <tr><td>ユーザID </td><td><input type="text" name="user" value="<?= $id ?>"></td></tr>
+        <tr><td>パスワード </td><td><input type="password" name="pass" value=""></td></tr>
+        </table>
+        <p><input type="submit" value="ログイン"></p>
+    </form>
+</body>
+</html>
---
+++ logout.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+
+$_SESSION = array();
+$params = session_get_cookie_params();
+setcookie(session_name(), "", time() - 36000,
+ $params["path"], $params["domain"],
+ $params["secure"], $params["httponly"]);
+session_destroy();
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ログアウト</title>
+</head>
+<body>
+    <p>ログアウトしました</p>
+    <p><a href="login.php">ログインページへ</a></p>
+</body>
+</html>
